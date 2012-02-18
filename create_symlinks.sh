e866417fa35d82267b2d6a59f1392b521eee5ec1
tupton <thomasupton@gmail.com>
2012-02-18 16:29:27 -0600

    Add a small utility script to create symlinks to these dotfiles in a given directory.

---
+++ create_symlinks.sh
@@ -0,0 +1,18 @@
+# Make sure we are given a directory and that it's not a symlink
+DIRECTORY="$1"
+if [[ ! -d "${DIRECTORY}" || -L "${DIRECTORY}" ]]; then
+    echo "The target provided is not a directory: $DIRECTORY" 
+    exit 1
+fi
+
+# Create symlinks to config files
+for FILE in `pwd`/*
+do
+    BASE=`basename $FILE`
+
+    # Exclude the README and this script
+    if [[ "README.md" != "$BASE" && `basename $0` != "$BASE" ]]; then
+        echo "Linking $BASE to $DIRECTORY/.$BASE ..."
+        ln -s `pwd`/$BASE $DIRECTORY/.$BASE
+    fi
+done

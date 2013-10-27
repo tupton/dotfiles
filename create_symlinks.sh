7b7ddb2db6c38133c5e318197adcb2a9fe1b8620
Thomas Upton <thomasupton@gmail.com>
2013-10-27 14:29:17 -0500

    Use $HOME as the source instead of basename.
    Plus some whitespace cleanup.

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,16 +1,19 @@
 #! /bin/bash
 
+if [[ -z "$DOTFILES" ]]; then
+    DOTFILES=".dotfiles"
+fi
+SOURCE="$HOME/$DOTFILES"
+
 # Make sure we are given a directory and that it's not a symlink
 DESTINATION="$1"
-SOURCE="`dirname $0`"
-
 if [[ -z "$DESTINATION" ]]; then
     echo "No directory given; using $HOME"
     DESTINATION="$HOME"
 fi
 
 if [[ ! -d "$DESTINATION" || -L "$DESTINATION" ]]; then
-    echo "The target provided is not a directory: $DESTINATION" 
+    echo "The target provided is not a directory: $DESTINATION"
     exit 1
 fi
 
@@ -20,8 +23,8 @@ do
     BASE=`basename $FILE`
 
     # Exclude the README and this script
-    if [[ "README.md" != "$BASE" && `basename $0` != "$BASE" ]]; then
-        echo "Linking $BASE to $DESTINATION/.$BASE ..."
+    if [[ "README.md" != "$BASE" && "`basename $0`" != "$BASE" ]]; then
+        echo "Linking $SOURCE/$BASE to $DESTINATION/.$BASE ..."
         ln -s $SOURCE/$BASE $DESTINATION/.$BASE
     fi
 done

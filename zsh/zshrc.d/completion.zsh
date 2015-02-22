3378e0bfbdbb227ef89d77a63eb14f42aebde47f
Thomas Upton <tupton@datastax.com>
2015-02-22 13:00:44 -0600

    More concise conditionals.

--- zsh/zshrc.d/completion.zsh
+++ zsh/zshrc.d/completion.zsh
@@ -1,4 +1,2 @@
 # Homebrew doesn't write anything outside of `brew --prefix`
-if hash brew 2>/dev/null && [[ -d "$(brew --prefix)"/share/zsh-completions ]]; then
-    fpath=("$(brew --prefix)"share/zsh-completions $fpath)
-fi
+hash brew 2>/dev/null && [[ -d "$(brew --prefix)"/share/zsh-completions ]] && fpath=("$(brew --prefix)"share/zsh-completions $fpath)

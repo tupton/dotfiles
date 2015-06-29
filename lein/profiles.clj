31fc6e6054de80d787c6c81dd74b30d1dc88c3bf
Thomas Upton <tupton@datastax.com>
2015-06-29 09:56:57 -0500

    Use lein-ancient to upgrade profiles.clj
    `lein ancient upgrade-profiles :interactive lein/profiles.clj`

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,8 +1,8 @@
-{:user {:plugins [[jonase/eastwood "0.1.5"]
-                  [lein-cloverage "1.0.2"]
-                  [lein-exec "0.3.4"]
-                  [lein-ancient "0.5.5"]
-                  [lein-kibit "0.0.8"]]
-        :dependencies [[pjstadig/humane-test-output "0.6.0"]]
+{:user {:plugins [[jonase/eastwood "0.2.1"]
+                  [lein-cloverage "1.0.6"]
+                  [lein-exec "0.3.5"]
+                  [lein-ancient "0.6.7"]
+                  [lein-kibit "0.1.2"]]
+        :dependencies [[pjstadig/humane-test-output "0.7.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}

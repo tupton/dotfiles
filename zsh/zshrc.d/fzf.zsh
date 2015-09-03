feaa7bf045c8b84fdff18fd3b1a535a34cbda928
Thomas Upton <thomasupton@gmail.com>
2015-09-03 16:20:38 -0500

    Add more, better fzf defaults

--- zsh/zshrc.d/fzf.zsh
+++ zsh/zshrc.d/fzf.zsh
@@ -1,5 +1,7 @@
 # https://github.com/junegunn/fzf
 if [ -f "$HOME"/.fzf.zsh ]; then
-    export FZF_DEFAULT_COMMAND='(git ls-tree -r --name-only HEAD || find . -path "*/\.*" -prune -o -type f -print -o -type l -print | sed "s/^\.\///" ) 2> /dev/null'
     source "$HOME"/.fzf.zsh
+    export FZF_DEFAULT_COMMAND='(git ls-tree -r --name-only HEAD || find . -path "*/\.*" -prune -o -type f -print -o -type l -print | sed "s/^\.\///") 2> /dev/null'
+    export FZF_DEFAULT_OPTS="--extended"
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi

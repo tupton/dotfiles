bcdddb5be1cc5ba2547d21232a7d755172dc536d
Thomas Upton <thomasupton@gmail.com>
2015-11-06 11:23:05 -0600

    Temporary(?) eastwood config for lein

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -7,4 +7,17 @@
         :dependencies [[pjstadig/humane-test-output "0.7.0"]
                        [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]]
         :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]}}
+                     (pjstadig.humane-test-output/activate!)]}
+        :eastwood {:exclude-linters [:unlimited-use
+                                     :def-in-def
+                                     :misplaced-docstrings
+                                     :suspicious-test
+                                     :suspicious-expression
+                                     :unused-ret-vals
+                                     :wrong-arity
+                                     :wrong-ns-form
+                                     :constant-test
+                                     :local-shadows-var
+                                     :deprecations
+                                     :redefd-vars
+                                     :wrong-tag]}}

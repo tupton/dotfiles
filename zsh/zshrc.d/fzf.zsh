bc0a285233a94a850aec057cb045eec211ca3c98
Thomas Upton <thomasupton@gmail.com>
2016-02-24 19:24:34 -0600

    Invert script-terminating conditionals in zsh setup
    If we wrap the bulk of a script in a conditional, invert it and exit in
    the failing case so that the rest of the script isn't hidden in a block.

--- zsh/zshrc.d/fzf.zsh
+++ zsh/zshrc.d/fzf.zsh
@@ -1,7 +1,9 @@
 # https://github.com/junegunn/fzf
-if [ -f "$HOME"/.fzf.zsh ]; then
-    source "$HOME"/.fzf.zsh
-    export FZF_DEFAULT_COMMAND='(git ls-tree -r --name-only HEAD || find . -path "*/\.*" -prune -o -type f -print -o -type l -print | sed "s/^\.\///") 2> /dev/null'
-    export FZF_DEFAULT_OPTS="--extended"
-    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+if [ ! -f "$HOME"/.fzf.zsh ]; then
+    exit 0;
 fi
+
+source "$HOME"/.fzf.zsh
+export FZF_DEFAULT_COMMAND='(git ls-tree -r --name-only HEAD || find . -path "*/\.*" -prune -o -type f -print -o -type l -print | sed "s/^\.\///") 2> /dev/null'
+export FZF_DEFAULT_OPTS="--extended"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
--- zsh/zshrc.d/npm.zsh
+++ zsh/zshrc.d/npm.zsh
@@ -1,8 +1,10 @@
 # Use the closest node_modules/.bin to execute npm package binaries
 npm-exec() {
-    if hash npm 2>/dev/null; then
-        local bin="$1"
-        shift
-        "$(npm bin)"/"$bin" "$@"
+    if ! hash npm 2>/dev/null; then
+        return;
     fi
+
+    local bin="$1"
+    shift
+    "$(npm bin)"/"$bin" "$@"
 }

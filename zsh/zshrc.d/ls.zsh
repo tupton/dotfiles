2f0b65e948572c960563c9ba2d36b676e57974e4
Thomas Upton <thomasupton@gmail.com>
2016-10-26 22:22:27 +0000

    More robust ls color option determination

--- zsh/zshrc.d/ls.zsh
+++ zsh/zshrc.d/ls.zsh
@@ -1,10 +1,7 @@
 # Return appropriate options for ls
 lsopts() {
 
-    local lshelp lsopts colors
-
-    # Snarf the output of `ls --help` into a variable
-    lshelp=$(ls --help 2>&1)
+    local lsopts colors
 
     # Start collecting available options
     lsopts=()
@@ -13,10 +10,10 @@ lsopts() {
     lsopts+=(-F)
 
     # If the --color option is available and we have a terminal that supports
-    # at least eight colors, add --color=auto to the options
+    # at least eight colors, add platform-specific option for color
     colors=$(tput colors)
     if ((colors >= 8)); then
-        if [[ "$lshelp" == "*--color*" ]]; then
+        if ls --help |& grep "\-\-color" 2>&1 >/dev/null; then
             lsopts+=(--color=auto)
         else
             lsopts+=(-G)

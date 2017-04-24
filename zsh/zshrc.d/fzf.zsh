099d53d9bd2dbfc35c5642871bf1eb142a67473c
Thomas Upton <tupton@datastax.com>
2017-04-24 12:55:56 -0500

    Default to preview files when picking

--- zsh/zshrc.d/fzf.zsh
+++ zsh/zshrc.d/fzf.zsh
@@ -2,6 +2,6 @@
 if [ -f "$HOME"/.fzf.zsh ]; then
     source "$HOME"/.fzf.zsh
     export FZF_DEFAULT_COMMAND='(git ls-tree -r --name-only HEAD || command find -L . -mindepth 1 \( -path "*/\.*" -o -fstype "sysfs" -o -fstype "devfs" -o -fstype "devtmpfs" -o -fstype "proc" \) -prune -o -type f -print -o -type l -print | cut -b3- ) 2> /dev/null'
-    export FZF_DEFAULT_OPTS="--extended"
+    export FZF_DEFAULT_OPTS='--extended --preview="head -$LINES {}"'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi

7baf60397defc4defe1d0bd0b5625806ba67b5ab
Thomas Upton <tupton@datastax.com>
2017-08-23 18:02:33 -0500

    Add homebrew python to PATH

--- zsh/zshrc.d/brew.zsh
+++ zsh/zshrc.d/brew.zsh
@@ -1,4 +1,5 @@
 if hash brew 2>/dev/null; then
+    PATH="$(brew --prefix)"/opt/python/libexec/bin:"$PATH"
     hash node 2>/dev/null || PATH="$(brew --prefix)"/opt/node@6/bin:"$PATH"
     hash thrift 2>/dev/null || PATH="$(brew --prefix)"/opt/thrift@l.90/bin:"$PATH"
     PATH="$(brew --prefix)"/bin:"$PATH"

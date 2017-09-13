8662314a52e6ffc812c787cc3155035cfb0f3499
Thomas Upton <tupton@datastax.com>
2017-09-13 10:57:11 -0500

    Revert accidental path typo

--- zsh/zshrc.d/brew.zsh
+++ zsh/zshrc.d/brew.zsh
@@ -1,7 +1,7 @@
 if hash brew 2>/dev/null; then
     PATH="$(brew --prefix)"/opt/python/libexec/bin:"$PATH"
     hash node 2>/dev/null || PATH="$(brew --prefix)"/opt/node@6/bin:"$PATH"
-    hash thrift 2>/dev/null || PATH="$(brew --prefix)"/opt/thrift@l.90/bin:"$PATH"
+    hash thrift 2>/dev/null || PATH="$(brew --prefix)"/opt/thrift@0.90/bin:"$PATH"
     PATH="$(brew --prefix)"/bin:"$PATH"
     export PATH
 fi

770819fbd1cefb543075dd01fc84d69e9a258649
Thomas Upton <tupton@datastax.com>
2017-09-19 14:14:54 -0500

    Consolidate brew-only path augmentation

--- zsh/zshrc.d/brew.zsh
+++ zsh/zshrc.d/brew.zsh
@@ -4,9 +4,9 @@ if hash brew 2>/dev/null; then
     hash thrift 2>/dev/null || PATH="$(brew --prefix)"/opt/thrift@0.90/bin:"$PATH"
     PATH="$(brew --prefix)"/bin:"$PATH"
     export PATH
-fi
 
-# Homebrew doesn't write anything outside of `brew --prefix`
-# https://github.com/Homebrew/brew/blob/master/docs/Homebrew-and-Python.md#site-packages-and-the-pythonpath
-hash brew 2>/dev/null && export PYTHONPATH="$(brew --prefix)/lib/python2.7/site-packages:$PYTHONPATH"
-export PYTHONPATH
+    # Homebrew doesn't write anything outside of `brew --prefix`
+    # https://github.com/Homebrew/brew/blob/master/docs/Homebrew-and-Python.md#site-packages-and-the-pythonpath
+    PYTHONPATH="$(brew --prefix)/lib/python2.7/site-packages:$PYTHONPATH"
+    export PYTHONPATH
+fi

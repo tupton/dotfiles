4a98b01de0232928dbc6c377f1ddce748bc1130c
Thomas Upton <tupton@datastax.com>
2017-09-28 18:28:43 -0500

    Add alias with correct PYTHONPATH set when using http+homebrew

--- zsh/zshrc.d/aliases.zsh
+++ zsh/zshrc.d/aliases.zsh
@@ -2,3 +2,5 @@ alias reload!="source ~/.zshrc"
 
 # If we're on Ubuntu, the name of the binary is ack-grep.
 hash ack-grep 2>/dev/null && alias ack=ack-grep
+
+hash brew 2>/dev/null && alias http='PYTHONPATH="/usr/local/lib/python3.6/site-packages" http'

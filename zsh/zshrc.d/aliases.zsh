c58865bdb11ee8deeecd126f70e73c7600810ba0
Thomas Upton <tupton@datastax.com>
2017-11-24 16:50:06 -0600

    Add an alias for rg with always-on options

--- zsh/zshrc.d/aliases.zsh
+++ zsh/zshrc.d/aliases.zsh
@@ -4,3 +4,5 @@ alias reload!="source ~/.zshrc"
 hash ack-grep 2>/dev/null && alias ack=ack-grep
 
 hash brew 2>/dev/null && alias http='PYTHONPATH="/usr/local/lib/python3.6/site-packages" http'
+
+hash rg 2>/dev/null && alias rg="rg --pretty --context=3 --smart-case --type-add='clj:include:clojure' --type-add='edn:*.edn' --type-add='gradle:*.gradle'"

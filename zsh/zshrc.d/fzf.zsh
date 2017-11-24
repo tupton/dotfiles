6b45b164e5caa461bf54ef2fa15fa1f5a633673c
Thomas Upton <tupton@datastax.com>
2017-11-24 16:25:52 -0600

    Fall back to fd instead of a complex find invocation for fzf file commands

--- zsh/zshrc.d/fzf.zsh
+++ zsh/zshrc.d/fzf.zsh
@@ -1,7 +1,7 @@
 # https://github.com/junegunn/fzf
 if [ -f "$HOME"/.fzf.zsh ]; then
     source "$HOME"/.fzf.zsh
-    export FZF_DEFAULT_COMMAND='git ls-tree -r --name-only HEAD || (set -o pipefail; (command find -L . -mindepth 1 \( -path "*/\.*" -o -fstype "sysfs" -o -fstype "devfs" -o -fstype "devtmpfs" -o -fstype "proc" \) -prune -o -type f -print -o -type l -print || command find -L . -mindepth 1 -path "*/\.*" -prune -o -type f -print -o -type l -print) ) 2> /dev/null | cut -b3- 2> /dev/null'
+    export FZF_DEFAULT_COMMAND='git ls-tree -r --name-only HEAD || fd --hidden --no-ignore --type f'
     export FZF_DEFAULT_OPTS='--extended --preview="head -$LINES {}"'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
     export FZF_CTRL_R_OPTS='--no-preview'

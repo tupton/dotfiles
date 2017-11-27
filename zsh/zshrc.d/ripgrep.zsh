21c092f2064e37f34e6dc146efb980b48e659691
Thomas Upton <tupton@datastax.com>
2017-11-27 16:18:26 -0600

    Add comment explaining why this is necessary

--- zsh/zshrc.d/ripgrep.zsh
+++ zsh/zshrc.d/ripgrep.zsh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
+# rg --context=3 -l still outputs the context separator (default "--") between file names. In order
+# to list files so that we can pass from a subshell to vim, we need to get rid of that context
+# separator. So, default to adding --context=3 to the list of options unless -l/--files-with-matches
+# is present.
 function rg() {
     local -a rgopts
     zparseopts -M -E -A opts -- l -files-with-matches=l

7bee5a8a823624c240026acd5f36b188221eeb02
Thomas Upton <tupton@datastax.com>
2017-12-23 11:04:53 -0500

    Redirect stderr in fzf default command

--- zsh/zshrc.d/fzf.zsh
+++ zsh/zshrc.d/fzf.zsh
@@ -1,7 +1,7 @@
 # https://github.com/junegunn/fzf
 if [ -f "$HOME"/.fzf.zsh ]; then
     source "$HOME"/.fzf.zsh
-    export FZF_DEFAULT_COMMAND='git ls-tree -r --name-only HEAD || fd --hidden --no-ignore --type f'
+    export FZF_DEFAULT_COMMAND='git ls-tree -r --name-only HEAD 2>/dev/null || fd --hidden --no-ignore --type f 2>/dev/null'
     export FZF_DEFAULT_OPTS='--extended --preview="head -$LINES {}"'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
     export FZF_CTRL_R_OPTS='--no-preview'

5efbbfff9f0f96d717c954a2607a3e08c770d4bf
Thomas Upton <thomasupton@gmail.com>
2018-03-05 08:51:32 -0600

    Add brew's bin at the end of PATH, after other additions

--- zsh/zshrc.d/brew.zsh
+++ zsh/zshrc.d/brew.zsh
@@ -1,8 +1,8 @@
 if hash brew 2>/dev/null; then
-    PATH="$(brew --prefix)"/bin:"$PATH"
     PATH="$(brew --prefix)"/opt/python@2/libexec/bin:"$PATH"
     hash node 2>/dev/null || PATH="$(brew --prefix)"/opt/node@6/bin:"$PATH"
     hash thrift 2>/dev/null || PATH="$(brew --prefix)"/opt/thrift@0.9/bin:"$PATH"
+    PATH="$PATH":"$(brew --prefix)"/bin
     export PATH
 
     # Homebrew doesn't write anything outside of `brew --prefix`

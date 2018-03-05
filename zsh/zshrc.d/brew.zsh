41719ffd58ac2c04a60705ef48af3d2c754ba3c8
Thomas Upton <thomasupton@gmail.com>
2018-03-04 21:28:21 -0600

    Homebrew path rejiggering: python2 has to come first; thrift@0.9 is more correct

--- zsh/zshrc.d/brew.zsh
+++ zsh/zshrc.d/brew.zsh
@@ -1,8 +1,8 @@
 if hash brew 2>/dev/null; then
-    PATH="$(brew --prefix)"/opt/python/libexec/bin:"$PATH"
-    hash node 2>/dev/null || PATH="$(brew --prefix)"/opt/node@6/bin:"$PATH"
-    hash thrift 2>/dev/null || PATH="$(brew --prefix)"/opt/thrift@0.90/bin:"$PATH"
     PATH="$(brew --prefix)"/bin:"$PATH"
+    PATH="$(brew --prefix)"/opt/python@2/libexec/bin:"$PATH"
+    hash node 2>/dev/null || PATH="$(brew --prefix)"/opt/node@6/bin:"$PATH"
+    hash thrift 2>/dev/null || PATH="$(brew --prefix)"/opt/thrift@0.9/bin:"$PATH"
     export PATH
 
     # Homebrew doesn't write anything outside of `brew --prefix`

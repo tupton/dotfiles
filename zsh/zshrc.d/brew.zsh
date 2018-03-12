73cace1ea3cf7b3fa9b4b54d6838618e337ec287
Thomas Upton <thomasupton@gmail.com>
2018-03-12 10:17:15 -0500

    Updated python@2 path

--- zsh/zshrc.d/brew.zsh
+++ zsh/zshrc.d/brew.zsh
@@ -1,5 +1,5 @@
 if hash brew 2>/dev/null; then
-    PATH="$(brew --prefix)"/opt/python@2/libexec/bin:"$PATH"
+    PATH="$(brew --prefix)"/opt/python@2/bin:"$PATH"
     PATH="$(brew --prefix)"/opt/ipython@5/bin:"$PATH"
     hash node 2>/dev/null || PATH="$(brew --prefix)"/opt/node@6/bin:"$PATH"
     hash thrift 2>/dev/null || PATH="$(brew --prefix)"/opt/thrift@0.9/bin:"$PATH"

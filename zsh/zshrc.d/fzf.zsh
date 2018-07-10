d47342ac4d8c157c135c2785f4befc5d782cf55f
Thomas Upton <thomasupton@gmail.com>
2018-07-10 15:53:40 -0500

    Use ls colors when previewing directories

--- zsh/zshrc.d/fzf.zsh
+++ zsh/zshrc.d/fzf.zsh
@@ -8,5 +8,5 @@ if [ -f "$HOME"/.fzf.zsh ]; then
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
     export FZF_CTRL_R_OPTS='--no-preview'
     export FZF_ALT_C_COMMAND='fd --hidden --no-ignore --type d'
-    export FZF_ALT_C_OPTS='--preview="command ls -al {}"'
+    export FZF_ALT_C_OPTS='--preview="CLICOLOR_FORCE=1 command ls -F -G -al {}"'
 fi

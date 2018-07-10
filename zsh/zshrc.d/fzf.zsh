7a3099f13ec52833dc1ef7d9c6a83ce667c7767d
Thomas Upton <thomasupton@gmail.com>
2018-07-10 15:54:49 -0500

    Don't show hidden or ignored directories when inside a git tree

--- zsh/zshrc.d/fzf.zsh
+++ zsh/zshrc.d/fzf.zsh
@@ -7,6 +7,6 @@ if [ -f "$HOME"/.fzf.zsh ]; then
         (highlight -O ansi -l {} || head -500 {}) 2> /dev/null | head -$LINES"'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
     export FZF_CTRL_R_OPTS='--no-preview'
-    export FZF_ALT_C_COMMAND='fd --hidden --no-ignore --type d'
+    export FZF_ALT_C_COMMAND='fd $( [[ $(git rev-parse --is-inside-work-tree) 2>/dev/null ]] || echo "--hidden --no-ignore" ) --type d'
     export FZF_ALT_C_OPTS='--preview="CLICOLOR_FORCE=1 command ls -F -G -al {}"'
 fi

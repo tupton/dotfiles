71623d21574eaeb3fb3c0a8223cd8d67ebd81b26
Thomas Upton <thomasupton@gmail.com>
2018-07-10 15:53:25 -0500

    Use `highlight` if available to preview files with fzf

--- zsh/zshrc.d/fzf.zsh
+++ zsh/zshrc.d/fzf.zsh
@@ -2,7 +2,9 @@
 if [ -f "$HOME"/.fzf.zsh ]; then
     source "$HOME"/.fzf.zsh
     export FZF_DEFAULT_COMMAND='git ls-tree -r --name-only HEAD 2>/dev/null || fd --hidden --no-ignore --type f 2>/dev/null'
-    export FZF_DEFAULT_OPTS='--extended --preview="head -$LINES {}"'
+    export FZF_DEFAULT_OPTS='--extended --preview=" [[ $(file --mime {}) =~ binary ]] &&
+        echo {} is a binary file ||
+        (highlight -O ansi -l {} || head -500 {}) 2> /dev/null | head -$LINES"'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
     export FZF_CTRL_R_OPTS='--no-preview'
     export FZF_ALT_C_COMMAND='fd --hidden --no-ignore --type d'

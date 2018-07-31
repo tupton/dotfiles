f1cd1bbabf1b5a41e51579002f027e0ffbfd0061
Thomas Upton <thomasupton@gmail.com>
2018-07-30 21:57:30 -0500

    Add shortcut for starting vim+codi

---
+++ zsh/zshrc.d/functions.zsh
@@ -0,0 +1,12 @@
+# Start vim with codi, the interactive code scratchpad
+codi() {
+  local syntax="${1:-python}"
+  [[ $# -gt 0 ]] && shift
+  vim -c "let g:startify_disable_at_vimenter = 1 |\
+    set bt=nofile ls=0 noru nonu nornu |\
+    hi ColorColumn ctermbg=NONE |\
+    hi VertSplit ctermbg=NONE |\
+    hi NonText ctermfg=0 |\
+    Codi $syntax" "$@"
+}
+

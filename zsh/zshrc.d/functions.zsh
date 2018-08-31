bbd02014c70a630ec42dc30d9fee0252da55bb9c
Thomas Upton <thomasupton@gmail.com>
2018-08-31 12:27:40 -0500

    Convenience function for converting a video to a gif

--- zsh/zshrc.d/functions.zsh
+++ zsh/zshrc.d/functions.zsh
@@ -10,3 +10,13 @@ codi() {
     Codi $syntax" "$@"
 }
 
+# Make a gif from an input movie
+gif() {
+    local input="$1"
+    local width="${2:-1000}"
+
+    local output="${input:t:r}.gif"
+    ffmpeg -i "$input" -filter_complex "[0:v] palettegen" palette.png
+    ffmpeg -i "$input" -i palette.png -filter_complex "[0:v] fps=15,scale=w=${width}:h=-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" "$output"
+    rm palette.png
+}

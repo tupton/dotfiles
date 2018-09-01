95b1f31265bc5a5a94ad68688a8685aad9ace8b0
Thomas Upton <thomasupton@gmail.com>
2018-08-31 22:55:37 -0500

    Use bat over highlight

--- zsh/zshrc.d/fzf.zsh
+++ zsh/zshrc.d/fzf.zsh
@@ -4,7 +4,7 @@ if [ -f "$HOME"/.fzf.zsh ]; then
     export FZF_DEFAULT_COMMAND='git ls-tree -r --name-only HEAD 2>/dev/null || fd --hidden --no-ignore --type f 2>/dev/null'
     export FZF_DEFAULT_OPTS='--extended --preview=" [[ $(file --mime {}) =~ binary ]] &&
         echo {} is a binary file ||
-        (highlight -O ansi -l {} || head -500 {}) 2> /dev/null | head -$LINES"'
+        (bat --color always {} || head -500 {}) 2> /dev/null | head -$LINES"'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
     export FZF_CTRL_R_OPTS='--no-preview'
     export FZF_ALT_C_COMMAND='fd $( [[ $(git rev-parse --is-inside-work-tree) 2>/dev/null ]] || echo "--hidden --no-ignore" ) --type d'

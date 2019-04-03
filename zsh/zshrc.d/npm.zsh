16cc8166f00797f187026c68d3ca77c0106997f2
Thomas Upton <thomasupton@gmail.com>
2019-04-03 12:22:16 -0500

    Set a dummy NPM_TOKEN to work around .npmrc's silly substitution rules

--- zsh/zshrc.d/npm.zsh
+++ zsh/zshrc.d/npm.zsh
@@ -11,3 +11,7 @@ npm-exec() {
 
 hash npm 2>/dev/null && path+=("$(npm bin -g)")
 export PATH
+
+# set a dummy NPM_TOKEN so that .npmrc files with ${NPM_TOKEN} don't fail on commands that don't need authentication.
+# this means that e.g. NPM_TOKEN=real-token-here npm install is necessary for commands that _do_ need authentication.
+export NPM_TOKEN=1

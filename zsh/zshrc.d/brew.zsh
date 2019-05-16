71370bb9597fa18c849959e13f2ee956cbb4046a
Thomas Upton <thomasupton@gmail.com>
2019-05-16 12:17:22 -0500

    Always point to node@10 for now
    node 12 breaks… lots of things.

--- zsh/zshrc.d/brew.zsh
+++ zsh/zshrc.d/brew.zsh
@@ -1,6 +1,6 @@
 if hash brew 2>/dev/null; then
     PATH="$(brew --prefix)"/opt/ipython@5/bin:"$PATH"
-    hash node 2>/dev/null || PATH="$(brew --prefix)"/opt/node@6/bin:"$PATH"
+    PATH="$(brew --prefix)"/opt/node@10/bin:"$PATH"
     hash thrift 2>/dev/null || PATH="$(brew --prefix)"/opt/thrift@0.9/bin:"$PATH"
     PATH="$PATH":"$(brew --prefix)"/bin
     export PATH

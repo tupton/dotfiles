e0f546a59b9b6cdf6f09cffb305bf1b85fd26238
Thomas Upton <thomasupton@gmail.com>
2019-06-07 11:20:25 -0500

    Add ruby+gems to PATH
    And use `brew --prefix <package>` for better prefix path gathering.

--- zsh/zshrc.d/brew.zsh
+++ zsh/zshrc.d/brew.zsh
@@ -1,6 +1,8 @@
 if hash brew 2>/dev/null; then
-    PATH="$(brew --prefix)"/opt/ipython@5/bin:"$PATH"
-    PATH="$(brew --prefix)"/opt/node@10/bin:"$PATH"
+    PATH="$(brew --prefix ipython@5)"/bin:"$PATH"
+    PATH="$(brew --prefix node@10)"/bin:"$PATH"
+    PATH="$(brew --prefix ruby)"/bin:"$PATH"
+    PATH="$(gem environment gemdir)"/bin:"$PATH"
     hash thrift 2>/dev/null || PATH="$(brew --prefix)"/opt/thrift@0.9/bin:"$PATH"
     PATH="$PATH":"$(brew --prefix)"/bin
     export PATH

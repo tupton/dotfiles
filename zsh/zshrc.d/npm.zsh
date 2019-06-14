a59760d9dd5a6d65f29ad7b96dd744b8d186345a
Thomas Upton <thomasupton@gmail.com>
2019-06-14 11:35:19 -0500

    Dummy NPM_EMAIL for .npmrc that needs it

--- zsh/zshrc.d/npm.zsh
+++ zsh/zshrc.d/npm.zsh
@@ -15,3 +15,6 @@ export PATH
 # set a dummy NPM_TOKEN so that .npmrc files with ${NPM_TOKEN} don't fail on commands that don't need authentication.
 # this means that e.g. NPM_TOKEN=real-token-here npm install is necessary for commands that _do_ need authentication.
 export NPM_TOKEN=1
+
+# apply the same process to NPM_EMAIL: use a dummy value, but provide a real one for commands that need it
+export NPM_EMAIL=1

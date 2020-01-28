d9273547df26b994173554cdbb2ed0f8f605205b
Thomas Upton <thomasupton@gmail.com>
2020-01-28 12:40:16 -0600

    Add perl module bin dir to PATH

--- zsh/zshrc.d/brew.zsh
+++ zsh/zshrc.d/brew.zsh
@@ -3,6 +3,7 @@ if hash brew 2>/dev/null; then
     PATH=/usr/local/opt/node@10/bin:"$PATH"
     PATH=/usr/local/opt/ruby/bin:"$PATH"
     PATH=/usr/local/lib/ruby/gems/2.6.0//bin:"$PATH"
+    PATH=/usr/local/Cellar/perl/5.30.1/bin/:"$PATH"
     hash thrift 2>/dev/null || PATH=/usr/local/opt/thrift@0.9/bin:"$PATH"
     export PATH
 

67874f425c1b645387b387567ac2c74112fd3d90
Thomas Upton <thomasupton@gmail.com>
2020-01-28 12:40:36 -0600

    More idiomatic zsh path addition

--- zsh/zshrc.d/brew.zsh
+++ zsh/zshrc.d/brew.zsh
@@ -1,10 +1,11 @@
 if hash brew 2>/dev/null; then
-    PATH=/usr/local/opt/ipython@5/bin:"$PATH"
-    PATH=/usr/local/opt/node@10/bin:"$PATH"
-    PATH=/usr/local/opt/ruby/bin:"$PATH"
-    PATH=/usr/local/lib/ruby/gems/2.6.0//bin:"$PATH"
-    PATH=/usr/local/Cellar/perl/5.30.1/bin/:"$PATH"
-    hash thrift 2>/dev/null || PATH=/usr/local/opt/thrift@0.9/bin:"$PATH"
+    path=( /usr/local/opt/ipython@5/bin \
+        /usr/local/opt/node@10/bin \
+        /usr/local/opt/ruby/bin \
+        /usr/local/lib/ruby/gems/2.6.0//bin \
+        /usr/local/Cellar/perl/5.30.1/bin/ \
+        "$path[@]")
+    hash thrift 2>/dev/null || path=(/usr/local/opt/thrift@0.9/bin "$path[@]")
     export PATH
 
     unset PYTHONPATH

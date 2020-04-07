4b05cfc509586a90c3c16b831920722aa1885e5e
Thomas Upton <thomasupton@gmail.com>
2020-04-07 15:07:37 -0500

    Versionless brew paths where possible

--- zsh/zshrc.d/brew.zsh
+++ zsh/zshrc.d/brew.zsh
@@ -2,8 +2,9 @@ if hash brew 2>/dev/null; then
     path=( /usr/local/opt/ipython@5/bin \
         /usr/local/opt/node@10/bin \
         /usr/local/opt/ruby/bin \
-        /usr/local/lib/ruby/gems/2.6.0//bin \
-        /usr/local/Cellar/perl/5.30.1/bin/ \
+        /usr/local/lib/ruby/gems/2.7.0/bin \
+        /usr/local/opt/perl/bin \
+        /usr/local/opt/php/bin \
         "$path[@]")
     hash thrift 2>/dev/null || path=(/usr/local/opt/thrift@0.9/bin "$path[@]")
     export PATH

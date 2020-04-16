b1be4456544299a332c812e942a0a47396edf5dc
Thomas Upton <tom@shipwell.com>
2020-04-16 15:54:52 -0500

    Add homebrew unversioned python symlinks to path

--- zsh/zshrc.d/brew.zsh
+++ zsh/zshrc.d/brew.zsh
@@ -5,6 +5,7 @@ if hash brew 2>/dev/null; then
         /usr/local/lib/ruby/gems/2.7.0/bin \
         /usr/local/opt/perl/bin \
         /usr/local/opt/php/bin \
+        /usr/local/opt/python/libexec/bin \
         "$path[@]")
     hash thrift 2>/dev/null || path=(/usr/local/opt/thrift@0.9/bin "$path[@]")
     export PATH

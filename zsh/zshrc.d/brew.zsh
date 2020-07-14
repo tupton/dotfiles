92f40f73427a8e5591177a67b4c691b7276f10d5
Thomas Upton <thomasupton@gmail.com>
2020-07-13 22:10:21 -0500

    Use latest/normally-located node by default

--- zsh/zshrc.d/brew.zsh
+++ zsh/zshrc.d/brew.zsh
@@ -1,6 +1,5 @@
 if hash brew 2>/dev/null; then
     path=( /usr/local/opt/ipython@5/bin \
-        /usr/local/opt/node@12/bin \
         /usr/local/opt/ruby/bin \
         /usr/local/lib/ruby/gems/2.7.0/bin \
         /usr/local/opt/perl/bin \

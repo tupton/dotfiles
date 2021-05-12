16629c18fa2ba6a0a9eaff4a86922ceff320846d
Thomas Upton <thomasupton@gmail.com>
2021-05-11 23:28:48 -0500

    Add key-bindings for the fuzzy-* helpers

--- zsh/zshrc.d/keys.zsh
+++ zsh/zshrc.d/keys.zsh
@@ -26,3 +26,56 @@ bindkey '^[[1~' beginning-of-line
 bindkey '^[[4~' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^?' backward-delete-char
+
+# https://gist.github.com/junegunn/8b572b8d4b5eddd8b85e5f4d40f17236
+is_in_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
+}
+
+_gf() {
+  is_in_git_repo || return
+  git fuzzy-files
+}
+
+_gb() {
+  is_in_git_repo || return
+  git fuzzy-branch
+}
+
+_gt() {
+  is_in_git_repo || return
+  git fuzzy-tag
+}
+
+_gh() {
+  is_in_git_repo || return
+  git fuzzy-hash
+}
+
+_gr() {
+  is_in_git_repo || return
+  git fuzzy-remote
+}
+
+_gs() {
+  is_in_git_repo || return
+  git fuzzy-stash
+}
+
+join-lines() {
+  local item
+  while read item; do
+    echo -n "${(q)item} "
+  done
+}
+
+bind-git-helper() {
+  local c
+  for c in $@; do
+    eval "fzf-g$c-widget() { local result=\$(_g$c | join-lines); zle reset-prompt; LBUFFER+=\$result }"
+    eval "zle -N fzf-g$c-widget"
+    eval "bindkey '^g^$c' fzf-g$c-widget"
+  done
+}
+bind-git-helper f b t h r s
+unset -f bind-git-helper

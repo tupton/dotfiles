8f77c547a83b903dea2ef62547e525b79ffe5b69
Thomas Upton <thomasupton@gmail.com>
2023-09-18 22:05:32 -0500

    Better tmux-fzf integration
    - Use centered pane
    - Numbers for file previews
    - Preview toggle options for files and commands

--- zsh/zshrc.d/fzf.zsh
+++ zsh/zshrc.d/fzf.zsh
@@ -8,14 +8,26 @@ if hash 2>/dev/null fzf; then
     export FZF_DEFAULT_OPTS="--ansi"
 
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-    export FZF_CTRL_T_OPTS="--preview-window right:60% --preview '(bat --color=always --style=header,grid --line-range :300 {} || cat {} || tree -C {}) 2>/dev/null | head -300'"
+    # CTRL-/ to toggle preview window position
+    export FZF_CTRL_T_OPTS="
+      --preview='(bat --color=always --style=header,grid,numbers --line-range :300 {} || cat {} || tree -C {}) 2>/dev/null | head -300'
+      --preview-window=right:50%:border-left
+      --bind='ctrl-/:change-preview-window(down|hidden|)'
+      --layout=reverse
+      --multi"
 
-    export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
+    # CTRL-/ to toggle small preview window to see the full command
+    # CTRL-Y to copy the command into clipboard using pbcopy
+    export FZF_CTRL_R_OPTS="
+      --preview='echo {}' --preview-window=up:3:hidden:wrap
+      --bind='ctrl-/:toggle-preview'
+      --bind='ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'"
 
     export FZF_ALT_C_COMMAND='fd --hidden --type directory --color=always'
     export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
 
     export FZF_TMUX=1
+    export FZF_TMUX_OPTS="-p80%,60%"
 
     # Explicitly enable the  fzf-tab oh-my-zsh extension if it is loaded
     typeset -f enable-fzf-tab >/dev/null && enable-fzf-tab

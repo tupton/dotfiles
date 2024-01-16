fbf5a11a573d74a9e1c403bd7c4f9a01b663e733
Thomas Upton <thomasupton@gmail.com>
2024-01-16 11:32:45 -0600

    Remove unused codi function

--- zsh/zshrc.d/functions.zsh
+++ zsh/zshrc.d/functions.zsh
@@ -1,15 +1,3 @@
-# Start vim with codi, the interactive code scratchpad
-codi() {
-  local syntax="${1:-python}"
-  [[ $# -gt 0 ]] && shift
-  vim -c "let g:startify_disable_at_vimenter = 1 |\
-    set bt=nofile ls=0 noru nonu nornu |\
-    hi ColorColumn ctermbg=NONE |\
-    hi VertSplit ctermbg=NONE |\
-    hi NonText ctermfg=0 |\
-    Codi $syntax" "$@"
-}
-
 # Make a gif from an input movie
 gif() {
     local input="$1"

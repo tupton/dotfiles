74903c39047b52e03399b5b118c9fcb8f68e8c25
Thomas Upton <thomasupton@gmail.com>
2024-01-16 10:54:24 -0600

    Remove unused aliases

--- zsh/zshrc.d/aliases.zsh
+++ zsh/zshrc.d/aliases.zsh
@@ -1,9 +1,3 @@
 alias reload!="source ~/.zshrc"
 
-# If we're on Ubuntu, the name of the binary is ack-grep.
-hash ack-grep 2>/dev/null && alias ack=ack-grep
-
-unalias gr
-alias gr='"$(git rev-parse --show-toplevel)"/gradlew'
-
 [[ -x /Applications/Tailscale.app/Contents/MacOS/Tailscale ]] && alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"

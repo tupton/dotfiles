5c7e202c8e2c57e7b65b65638d8b9a8ca57e78ec
Thomas Upton <thomasupton@gmail.com>
2024-02-26 10:23:02 -0600

    Remove now-unused config files that were only touching $path

--- zsh/zshrc.d/rust.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-path=(~/.cargo/bin "$path[@]")
-export PATH
--- zsh/zshrc.d/yarn.zsh
+++
@@ -1,4 +0,0 @@
-path=( $HOME/.yarn/bin \
-  $HOME/.config/yarn/global/node_modules/.bin
-  "$path[@]")
-export PATH

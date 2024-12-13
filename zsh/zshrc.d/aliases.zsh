2ac298025a209b3da215275cab6d1e8882ee5e74
Thomas Upton <thomasupton@gmail.com>
2024-12-12 22:43:01 -0600

    Prefer nvim if available

--- zsh/zshrc.d/aliases.zsh
+++ zsh/zshrc.d/aliases.zsh
@@ -1,3 +1,5 @@
 alias reload!="source ~/.zshrc"
 
 [[ -x /Applications/Tailscale.app/Contents/MacOS/Tailscale ]] && alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+
+hash nvim 2>/dev/null && alias vim=nvim

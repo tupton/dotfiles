8e0e8998424b547af4153bc5173d52c35ff318dd
Thomas Upton <tom@wizehire.com>
2025-04-17 10:13:41 -0500

    Update session switching fzf widget
    - Remove ability to kill sessions from session switcher
    - Hide the attached (current) session in the switcher
    - Hide duplicate directories in the switcher
    - Use icons in session list

--- zsh/zshrc.d/functions.zsh
+++ zsh/zshrc.d/functions.zsh
@@ -22,19 +22,3 @@ base64dec() {
   local input="${1}"
   echo "$input" | base64 --decode -i -
 }
-
-sesh-connect() {
-  hash sesh 2>/dev/null || return
-  sesh connect "$(
-    sesh list | fzf-tmux -p 55%,60% \
-      --no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
-      --header '  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find' \
-      --bind 'tab:down,btab:up' \
-      --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list)' \
-      --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t)' \
-      --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c)' \
-      --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z)' \
-      --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
-      --bind 'ctrl-d:execute(tmux kill-session -t {})+change-prompt(⚡  )+reload(sesh list)'
-  )"
-}

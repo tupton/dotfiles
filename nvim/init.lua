5be55b3dcb6235f56d25bbaee3b2a3d04a3fd82f
Thomas Upton <tom@wizehire.com>
2025-04-22 14:45:45 -0500

    Sync nvim config to dotfiles
    Including the new Mason lock file.

--- nvim/init.lua
+++ nvim/init.lua
@@ -190,7 +190,7 @@ vim.opt.formatoptions:append { t = true, c = true, r = true, o = true, n = true 
 vim.opt.equalalways = false
 
 -- Enable spell checking
-vim.opt.spell = true
+vim.opt.spell = false
 vim.opt.spelllang = 'en_us'
 
 -- Less busy diff views
@@ -245,6 +245,8 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.breakindent = true
     vim.opt_local.cpoptions:append 'n'
     vim.opt_local.showbreak = '» '
+    -- 2 to hide conceal text
+    vim.opt_local.conceallevel = 2
   end,
 })
 
@@ -291,6 +293,7 @@ require('lazy').setup({
 
   -- :Gdiff, :Gcommit, etc.
   'tpope/vim-fugitive',
+
   -- :GBrowse with Github
   'tpope/vim-rhubarb',
 
@@ -363,8 +366,15 @@ require('lazy').setup({
     event = 'VimEnter',
     config = function()
       require('fzf-lua').setup {
-        { 'fzf-native' },
+        { 'fzf-vim', 'fzf-tmux' },
         fzf_colors = true,
+        winopts = {
+          preview = {
+            default = 'bat',
+            hidden = false,
+            horizontal = 'right:40%',
+          },
+        },
         keymap = {
           fzf = {
             -- inherits the default keymaps
@@ -483,14 +493,17 @@ require('lazy').setup({
 
   -- LSP Plugins
   {
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime, and plugins
+    -- used for completion, annotations and signatures of Neovim APIs
     'folke/lazydev.nvim',
     ft = 'lua',
     opts = {
       library = {
         -- Load luvit types when the `vim.uv` word is found
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        {
+          path = '${3rd}/luv/library',
+          words = { 'vim%.uv' },
+        },
       },
     },
   },
@@ -499,15 +512,16 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
-      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependents
+      { 'williamboman/mason.nvim', opts = {} }, -- NOTE: Must be loaded before dependents
+      { 'zapling/mason-lock.nvim', opts = {} },
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
 
-      -- Allows extra capabilities provided by nvim-cmp
-      'hrsh7th/cmp-nvim-lsp',
+      -- Allows extra capabilities provided by blink.cmp
+      'saghen/blink.cmp',
     },
     config = function()
       -- Brief aside: **What is LSP?**
@@ -554,46 +568,48 @@ require('lazy').setup({
 
           local fzf = require 'fzf-lua'
 
+          -- Rename the variable under your cursor.
+          --  Most Language Servers support renaming across files, etc.
+          map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+          -- Execute a code action, usually your cursor needs to be on top of an error
+          -- or a suggestion from your LSP for this to activate.
+          map('gra', fzf.lsp_code_actions, 'Code [A]ction', { 'n', 'x' })
+
+          -- Find references for the word under your cursor.
+          map('grr', fzf.lsp_references, '[G]oto [R]eferences')
+
+          -- Jump to the implementation of the word under your cursor.
+          --  Useful when your language has ways of declaring types without an actual implementation.
+          map('gri', fzf.lsp_implementations, '[G]oto [I]mplementation')
+
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map('gd', function()
+          map('grd', function()
             fzf.lsp_definitions { jump1 = true }
           end, '[G]oto [D]efinition')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
-          map('gD', function()
+          map('grD', function()
             fzf.lsp_declarations { jump1 = true }
           end, '[G]oto [D]eclaration')
 
-          -- Find references for the word under your cursor.
-          map('gr', fzf.lsp_references, '[G]oto [R]eferences')
+          -- Fuzzy find all the symbols in your current document.
+          --  Symbols are things like variables, functions, types, etc.
+          map('gO', fzf.lsp_document_symbols, 'Document Symbols')
 
-          -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gI', fzf.lsp_implementations, '[G]oto [I]mplementation')
+          -- Fuzzy find all the symbols in your current workspace.
+          --  Similar to document symbols, except searches over your entire project.
+          map('gW', fzf.lsp_live_workspace_symbols, '[W]orkspace Symbols')
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map('<leader>D', fzf.lsp_typedefs, 'Type [D]efinition')
-
-          -- Fuzzy find all the symbols in your current document.
-          --  Symbols are things like variables, functions, types, etc.
-          map('<leader>ds', fzf.lsp_document_symbols, '[D]ocument [S]ymbols')
-
-          -- Fuzzy find all the symbols in your current workspace.
-          --  Similar to document symbols, except searches over your entire project.
-          map('<leader>ws', fzf.lsp_live_workspace_symbols, '[W]orkspace [S]ymbols')
-
-          -- Rename the variable under your cursor.
-          --  Most Language Servers support renaming across files, etc.
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-          -- Execute a code action, usually your cursor needs to be on top of an error
-          -- or a suggestion from your LSP for this to activate.
-          map('<leader>ca', fzf.lsp_code_actions, '[C]ode [A]ction', { 'n', 'x' })
+          map('grt', function()
+            fzf.lsp_typedefs { jump1 = true }
+          end, '[T]ype Definition')
         end,
       })
 
@@ -628,10 +644,9 @@ require('lazy').setup({
 
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
-      --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
+      --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -694,6 +709,15 @@ require('lazy').setup({
         css_variables = {},
         sqlls = {},
         buf_ls = {},
+        harper_ls = {
+          settings = {
+            ['harper-ls'] = {
+              linters = {
+                SentenceCapitalization = false,
+              },
+            },
+          },
+        },
       }
 
       -- Ensure the servers and tools above are installed
@@ -739,7 +763,24 @@ require('lazy').setup({
   {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
+    opts = {
+      settings = {
+        tsserver_file_preferences = {
+          importModuleSpecifierPreference = 'non-relative',
+          includeInlayParameterNameHints = 'all',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
+        jsx_close_tag = {
+          enabled = true,
+        },
+      },
+    },
   },
   { -- Autoformat
     'stevearc/conform.nvim',
@@ -802,90 +843,89 @@ require('lazy').setup({
   },
 
   { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
+    'saghen/blink.cmp',
+    event = 'VimEnter',
+    version = '1.*',
     dependencies = {
-      -- Adds other completion capabilities.
-      --  nvim-cmp does not ship with all sources by default. They are split
-      --  into multiple repos for maintenance purposes.
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-      'f3fora/cmp-spell',
+      { 'supermaven-inc/supermaven-nvim', opts = {} },
+      'huijiro/blink-cmp-supermaven',
+      'ribru17/blink-cmp-spell',
     },
-    config = function()
-      -- See `:help cmp`
-      local cmp = require 'cmp'
-
-      cmp.setup {
-        completion = { completeopt = 'menu,menuone,noinsert' },
-
-        -- For an understanding of why these mappings were
-        -- chosen, you will need to read `:help ins-completion`
+    --- @module 'blink.cmp'
+    --- @type blink.cmp.Config
+    opts = {
+      keymap = {
+        -- 'default' (recommended) for mappings similar to built-in completions
+        --   <c-y> to accept ([y]es) the completion.
+        --    This will auto-import if your LSP supports it.
+        --    This will expand snippets if the LSP sent a snippet.
+        -- 'super-tab' for tab to accept
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        --
+        -- For an understanding of why the 'default' preset is recommended,
+        -- you will need to read `:help ins-completion`
         --
         -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        mapping = cmp.mapping.preset.insert {
-          -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
+        --
+        -- All presets have the following mappings:
+        -- <tab>/<s-tab>: move to right/left of your snippet expansion
+        -- <c-space>: Open menu or open docs if already open
+        -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
+        -- <c-e>: Hide menu
+        -- <c-k>: Toggle signature help
+        --
+        -- See :h blink-cmp-config-keymap for defining your own keymap
+        preset = 'default',
 
-          -- Scroll the documentation window [b]ack / [f]orward
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+      },
 
-          -- Accept ([y]es) the completion.
-          --  This will auto-import if your LSP supports it.
-          --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono',
+      },
 
-          -- If you prefer more traditional completion keymaps,
-          -- you can uncomment the following lines
-          ['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+      completion = {
+        -- By default, you may press `<c-space>` to show the documentation.
+        -- Optionally, set `auto_show = true` to show the documentation after a delay.
+        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      },
 
-          -- Manually trigger a completion from nvim-cmp.
-          --  Generally you don't need this, because nvim-cmp will display
-          --  completions whenever it has completion options available.
-          ['<C-Space>'] = cmp.mapping.complete {},
-
-          -- Think of <c-l> as moving to the right of your snippet expansion.
-          --  So if you have a snippet that's like:
-          --  function $name($args)
-          --    $body
-          --  end
-          --
-          -- <c-l> will move you to the right of each of the expansion locations.
-          -- <c-h> is similar, except moving you backwards.
-          -- ['<C-l>'] = cmp.mapping(function()
-          --   if luasnip.expand_or_locally_jumpable() then
-          --     luasnip.expand_or_jump()
-          --   end
-          -- end, { 'i', 's' }),
-          -- ['<C-h>'] = cmp.mapping(function()
-          --   if luasnip.locally_jumpable(-1) then
-          --     luasnip.jump(-1)
-          --   end
-          -- end, { 'i', 's' }),
-
-          -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-          --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-        },
-        sources = {
-          {
-            name = 'lazydev',
-            -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-            group_index = 0,
+      sources = {
+        default = { 'lsp', 'supermaven', 'path', 'buffer', 'spell' },
+        providers = {
+          supermaven = {
+            name = 'supermaven',
+            module = 'blink-cmp-supermaven',
+            async = true,
           },
-          { name = 'supermaven' },
-          { name = 'nvim_lsp' },
-          { name = 'path' },
-          { name = 'nvim_lsp_signature_help' },
-          { name = 'spell' },
+          spell = {
+            name = 'spell',
+            module = 'blink-cmp-spell',
+            opts = {
+              -- If true, then completion candidates will be sorted using the same algorithm used by cmp-spell.
+              -- This can be helpful for sorting words that are partially typed. Otherwise, a custom sorting is used.
+              use_cmp_spell_sorting = true,
+            },
+          },
         },
-      }
-    end,
+      },
+
+      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
+      -- which automatically downloads a prebuilt binary when enabled.
+      --
+      -- By default, we use the Lua implementation instead, but you may enable
+      -- the rust implementation via `'prefer_rust_with_warning'`
+      --
+      -- See :h blink-cmp-config-fuzzy for more information
+      fuzzy = { implementation = 'lua' },
+
+      -- Shows a signature help window while you type arguments for a function
+      signature = { enabled = true },
+    },
   },
 
   { -- You can easily change to a different colorscheme.
@@ -905,7 +945,12 @@ require('lazy').setup({
   },
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = {} },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {},
+  },
 
   {
     'folke/trouble.nvim',
@@ -948,7 +993,7 @@ require('lazy').setup({
     config = function()
       require('twilight').setup {
         dimming = {
-          alpha = 0.15,
+          alpha = 0.75,
         },
         context = 30,
       }
@@ -1068,14 +1113,27 @@ require('lazy').setup({
       -- Highlight the word under the cursor
       require('mini.cursorword').setup()
 
-      -- f, t, F, T are repeatable motions
-      require('mini.jump').setup()
+      -- t, f, T, F are repeatable motions
+      -- Highlight the next motion for subsequent tfTF motions
+      require('mini.jump').setup {
+        delay = {
+          -- Delay between jump and highlighting all possible jumps
+          highlight = 1000,
+          -- Delay between jump and automatic stop if idle (no jump is done)
+          idle_stop = 3000,
+        },
+      }
 
       -- Jump to locations with <enter>
       require('mini.jump2d').setup {
         view = {
           dim = true,
           n_steps_ahead = 2,
+        },
+        allowed_lines = {
+          cursor_at = false,
+          blank = false,
+          fold = false,
         },
       }
 
@@ -1188,13 +1246,60 @@ require('lazy').setup({
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
-  {
-    'supermaven-inc/supermaven-nvim',
-    opts = {},
-  },
-
   -- Allow zip file reading
   'lbrayner/vim-rzip',
+
+  -- Edit and manage Obsidian notes and links
+  {
+    'obsidian-nvim/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = 'markdown',
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   'BufReadPre '
+    --     .. vim.fn.expand '~'
+    --     .. '/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian Vault/**/*.md',
+    --   'BufNewFile ' .. vim.fn.expand '~' .. '/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian Vault/**/*.md',
+    -- },
+    dependencies = {
+      -- Required.
+      'nvim-lua/plenary.nvim',
+
+      -- Optional.
+      'saghen/blink.cmp',
+      'ibhagwan/fzf-lua',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {
+      workspaces = {
+        {
+          name = 'work',
+          path = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian Vault',
+        },
+      },
+      completion = {
+        nvim_cmp = false,
+        blink = true,
+        min_chars = 1,
+      },
+    },
+  },
+  -- lazy.nvim
+  {
+    'm4xshen/hardtime.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+    },
+    opts = {
+      disable_mouse = false,
+      max_count = 5,
+      max_time = 500,
+    },
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and

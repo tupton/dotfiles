2b50206770ea145e3aec8fec5f42c13cf9c5e823
Thomas Upton <tom@wizehire.com>
2025-05-01 22:07:15 -0500

    Remove unused history-substring-search omz plugin

--- zsh/zshrc.d/keys.zsh
+++ zsh/zshrc.d/keys.zsh
@@ -4,15 +4,6 @@ bindkey '^S' history-incremental-search-forward
 bindkey '^P' history-search-backward
 bindkey '^N' history-search-forward
 
-# history substring search
-# bind UP and DOWN arrow keys
-# bindkey "$terminfo[kcuu1]" history-substring-search-up
-# bindkey "$terminfo[kcud1]" history-substring-search-down
-
-# bind k and j for VI mode
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
-
 # push current command to stack, run a different command, pop from stack
 bindkey '^Q' push-input
 

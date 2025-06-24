34b27e092c34e30cc93db7825662d7f918b18bb8
Thomas Upton <tom@wizehire.com>
2025-06-24 16:06:31 -0500

    Sync nvim config to dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -1304,8 +1304,9 @@ require('lazy').setup({
     opts = {
       stiffness = 0.8,
       trailing_stiffness = 0.5,
-      stiffness_insert_mode = 0.6,
-      trailing_stiffness_insert_mode = 0.6,
+      stiffness_insert_mode = 0.7,
+      trailing_stiffness_insert_mode = 0.7,
+      damping = 0.65,
       distance_stop_animating = 0.5,
     },
   },

ec0e09056710599ff7bf9f8cdb98c320d6aaa9e3
Thomas Upton <tom@wizehire.com>
2025-11-03 10:42:00 -0600

    Sync nvim config to dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -817,6 +817,114 @@ require('lazy').setup({
     },
   },
 
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'VimEnter',
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+      },
+    },
+  },
+
+  {
+    'folke/sidekick.nvim',
+    opts = {
+      -- add any options here
+      cli = {
+        mux = {
+          backend = 'tmux',
+          enabled = true,
+        },
+      },
+    },
+    keys = {
+      {
+        '<tab>',
+        function()
+          -- if there is a next edit, jump to it, otherwise apply it if any
+          if not require('sidekick').nes_jump_or_apply() then
+            return '<Tab>' -- fallback to normal tab
+          end
+        end,
+        expr = true,
+        desc = 'Goto/Apply Next Edit Suggestion',
+      },
+      {
+        '<c-.>',
+        function()
+          require('sidekick.cli').toggle()
+        end,
+        desc = 'Sidekick Toggle',
+        mode = { 'n', 't', 'i', 'x' },
+      },
+      {
+        '<leader>aa',
+        function()
+          require('sidekick.cli').toggle()
+        end,
+        desc = 'Sidekick Toggle CLI',
+      },
+      {
+        '<leader>as',
+        function()
+          require('sidekick.cli').select()
+        end,
+        -- Or to select only installed tools:
+        -- require("sidekick.cli").select({ filter = { installed = true } })
+        desc = 'Select CLI',
+      },
+      {
+        '<leader>ad',
+        function()
+          require('sidekick.cli').close()
+        end,
+        desc = 'Detach a CLI Session',
+      },
+      {
+        '<leader>at',
+        function()
+          require('sidekick.cli').send { msg = '{this}' }
+        end,
+        mode = { 'x', 'n' },
+        desc = 'Send This',
+      },
+      {
+        '<leader>af',
+        function()
+          require('sidekick.cli').send { msg = '{file}' }
+        end,
+        desc = 'Send File',
+      },
+      {
+        '<leader>av',
+        function()
+          require('sidekick.cli').send { msg = '{selection}' }
+        end,
+        mode = { 'x' },
+        desc = 'Send Visual Selection',
+      },
+      {
+        '<leader>ap',
+        function()
+          require('sidekick.cli').prompt()
+        end,
+        mode = { 'n', 'x' },
+        desc = 'Sidekick Select Prompt',
+      },
+      -- Example of a keybinding to open Claude directly
+      {
+        '<leader>ac',
+        function()
+          require('sidekick.cli').toggle { name = 'opencode', focus = true }
+        end,
+        desc = 'Sidekick Toggle OpenCode',
+      },
+    },
+  },
+
   -- Autoformat
   {
     'stevearc/conform.nvim',
@@ -885,8 +993,7 @@ require('lazy').setup({
     event = 'VimEnter',
     version = '1.*',
     dependencies = {
-      { 'supermaven-inc/supermaven-nvim', opts = {} },
-      'huijiro/blink-cmp-supermaven',
+      { 'fang2hou/blink-copilot', opts = {} },
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
@@ -928,15 +1035,15 @@ require('lazy').setup({
       completion = {
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
       },
 
       sources = {
-        default = { 'lsp', 'supermaven', 'path', 'buffer' },
+        default = { 'lsp', 'copilot', 'path', 'buffer' },
         providers = {
-          supermaven = {
-            name = 'supermaven',
-            module = 'blink-cmp-supermaven',
+          copilot = {
+            name = 'copilot',
+            module = 'blink-copilot',
             async = true,
           },
         },

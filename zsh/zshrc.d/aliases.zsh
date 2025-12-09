8a83d7a7a49f019a125fcfdd602715dc21863db0
Thomas Upton <tom@wizehire.com>
2025-12-08 22:08:23 -0600

    Add alias hl for [h]igh[l]ight with ripgrep

--- zsh/zshrc.d/aliases.zsh
+++ zsh/zshrc.d/aliases.zsh
@@ -3,3 +3,5 @@ alias reload!="source ~/.zshrc"
 [[ -x /Applications/Tailscale.app/Contents/MacOS/Tailscale ]] && alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 
 hash nvim 2>/dev/null && alias vim=nvim
+
+hash rg 2>/dev/null && alias hl="rg --no-config --passthru"

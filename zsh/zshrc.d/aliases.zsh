f72d60c5e548d22690e241ba68c552343715c0b0
Thomas Upton <tom@wizehire.com>
2026-01-09 09:10:13 -0600

    Prefer $HOME over ~ expansion
    ~ only gets expanded in certain contexts; $HOME is much more reliable.

--- zsh/zshrc.d/aliases.zsh
+++ zsh/zshrc.d/aliases.zsh
@@ -1,4 +1,4 @@
-alias reload!="source ~/.zshrc"
+alias reload!="source "$HOME"/.zshrc"
 
 [[ -x /Applications/Tailscale.app/Contents/MacOS/Tailscale ]] && alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 

bb029caf158d685dd54decab474ea9c4d917bb64
Thomas Upton <tom@wizehire.com>
2026-02-05 12:21:10 -0600

    Use gswt as alias to create tracking branch from a ticket name

--- zsh/zshrc.d/git.zsh
+++ zsh/zshrc.d/git.zsh
@@ -8,11 +8,14 @@ vimtree() {
     ${EDITOR:-vim} $(git fuzzy-files)
 }
 
-# check out a branch that tracks a remote branch
+# switch to a branch that tracks a remote branch
 # Used to create a branch from a Linear ticket branch name
-# [g]it [c]heck[o]ut with [t]racking
-gcot() {
+# [g]it [s][w]itch with [t]racking
+gswt() {
     local local_branch=${1:-$(pbpaste | tr -d '\n')}
     local remote_tracking_branch=${2:-origin/main}
     git switch -c ${local_branch} ${remote_tracking_branch}
 }
+# Alias with co for any lingering shell history
+# [g]it [c]heck[o]ut with [t]racking
+alias gcot=gswt
